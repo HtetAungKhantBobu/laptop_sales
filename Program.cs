2c628c676542fa08e72197b79941894fa7194d6a
Htet Aung Khant <bob.h@bettercast.io>
2024-01-02 15:16:48 +0630

    first commit

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+namespace LaptopSales
+{
+
+    class Laptop
+    {
+        public string brand;
+        public string model;
+
+        public int year;
+        public string os;
+        public bool is_touchscreen;
+        public double price;
+        public int stock;
+
+        public Laptop(
+            string lbrand,
+            string lmodel,
+            int lyear,
+            string los,
+            bool ltouch,
+            double lprice,
+            int lstock
+        )
+        {
+            brand = lbrand;
+            model = lmodel;
+            year = lyear;
+            os = los;
+            is_touchscreen = ltouch;
+            price = lprice;
+            stock = lstock;
+        }
+
+        public void sell()
+        {
+            if (stock < 1)
+            {
+                Console.WriteLine("Out of stock!");
+                return;
+            }
+            Console.WriteLine($"Sold 1 {brand} - {model}!");
+            stock--;
+        }
+
+        public void discount(double percent)
+        {
+            double discounted_price = price * (percent / 100);
+            Console.WriteLine($"Discounted price is {discounted_price}$!");
+        }
+    }
+    class LaptopSales
+    {
+        public static void Main(string[] args)
+        {
+
+            Laptop laptop1 = new Laptop("Apple", "Air M3", 2023, "Mac", false, 1400.00, 10);
+            Laptop laptop2 = new Laptop("Lenovo", "Thinkpad Carbon", 2023, "Windows", false, 2000.00, 10);
+            Laptop laptop3 = new Laptop("MSI", "MSI-XXX", 2023, "Windows", false, 1200.00, 2);
+
+            laptop3.sell();
+            laptop3.sell();
+            laptop3.sell();
+
+            laptop2.discount(50.0);
+        }
+    }
+}
---
+++ task.md
@@ -0,0 +1,19 @@
+# Task
+
+1. make a class, named `Laptop`, with the following members:
+   1. a string type brand
+   2. a string type model
+   3. an integer type year
+   4. a string type os
+   5. a boolean type is_touch_screen
+   6. a double type price
+   7. an integer type stock
+   8. a method called sell(), which will print `Sold 1 {brand} - {model}!` , and, reduce the stock value by 1
+      1. Handling the cases where there is no stock should be implemented if you have time
+   9. a method called discount() which will take `1 paramenter called percent` and will perform the following operations
+      1.  divide the percent with 100
+      2.  multiply the prce with the result from step 1
+      3.  print the resulted discounted price
+2. make a `constructor class` for the class `Laptop`, with appropriate parameters
+3. make 3 different `objects` using `Laptop` class
+   
